314c5865e52b9a61c8687d8edf9d4395d945f12d
Rizky Benggolo <travarilo@gmail.com>
2017-05-12 09:19:38 +0700

    cancro: init.qcom.post_boot: some tweak
    Change-Id: Ia4fcbe1cdaf9082632fbc234f41b9790224f865d

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -51,7 +51,7 @@ case "$target" in
         echo 1 > /sys/module/msm_pm/modes/cpu1/retention/idle_enabled
         echo 1 > /sys/module/msm_pm/modes/cpu2/retention/idle_enabled
         echo 1 > /sys/module/msm_pm/modes/cpu3/retention/idle_enabled
-        echo 1 > /sys/kernel/msm_thermal/enabled
+        echo Y > /sys/module/msm_thermal/parameters/enabled
         echo Y > /sys/module/clock_krait_8974/parameters/boost
         stop mpdecision
         echo 1 > /sys/devices/system/cpu/cpu1/online
@@ -72,16 +72,15 @@ case "$target" in
                 echo "interactive" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
                 echo "interactive" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
                 echo "interactive" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-                echo "20000 1400000:40000 1700000:20000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
-                echo 90 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
-                echo 1190400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+                echo "19000 1400000:39000 1700000:19000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+                echo 97 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+                echo 1497600 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
                 echo 1497600 > /sys/devices/system/cpu/cpufreq/interactive/input_boost_freq
-                echo 0 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
+                echo 1 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
                 echo "85 1500000:90 1800000:70" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
                 echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
-                echo 20 > /sys/module/cpu_boost/parameters/boost_ms
-                echo 1728000 > /sys/module/cpu_boost/parameters/sync_threshold
                 echo 100000 > /sys/devices/system/cpu/cpufreq/interactive/sampling_down_factor
+                echo 1728000 > /sys/module/cpu_boost/parameters/sync_threshold
                 echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
                 echo '1' > /sys/kernel/fast_charge/force_fast_charge
                 setprop ro.qualcomm.perf.cores_online 2
@@ -104,11 +103,7 @@ case "$target" in
                 echo 80 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load
             ;;
         esac
-        echo 268800 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-        echo 268800 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-        echo 268800 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
-        echo 268800 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq        
-        echo 1 > /sys/kernel/msm_thermal/enabled
+        echo Y > /sys/module/msm_thermal/parameters/enabled
         chown -h root.system /sys/devices/system/cpu/mfreq
         chmod -h 220 /sys/devices/system/cpu/mfreq
         chown -h root.system /sys/devices/system/cpu/cpu1/online
@@ -140,7 +135,7 @@ case "$target" in
         chown system.system /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
         chown system.system /sys/devices/system/cpu/cpu3/cpufreq/sys_cap_freq
         chown system.system /sys/devices/system/cpu/cpu3/cpufreq/thermal_cap_freq
-        echo 2 > /sys/devices/system/cpu/sched_mc_power_savings
+        echo 1 > /sys/devices/system/cpu/sched_mc_power_savings
         chown system.system /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
         chown system.system /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
         chown system.system /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
